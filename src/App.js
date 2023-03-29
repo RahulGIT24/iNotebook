6f6567f38f27aa3bd8a7762071153fd7ea12f205
Rahul <rg4005450@gmail.com>
2023-03-29 13:56:01 +0530

    Included context in app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from "./Components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
 import Alert from "./Components/Alert";
+import DarkState from "./context/darkmode/DarkState";
 
 function App() {
   const [alert, setAlert] = useState(null);
@@ -21,30 +22,32 @@ function App() {
   };
   return (
     <NoteState>
-      <Router>
-        <Navbar renderAlert={renderAlert}/>
-        <Alert alert={alert} />
-        <div className="container">
-          <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route
-              exact
-              path="/"
-              element={<Home renderAlert={renderAlert} />}
-            />
-            <Route
-              exact
-              path="/signup"
-              element={<Signup renderAlert={renderAlert} />}
-            />
-            <Route
-              exact
-              path="/login"
-              element={<Login renderAlert={renderAlert} />}
-            />
-          </Routes>
-        </div>
-      </Router>
+      <DarkState>
+        <Router>
+          <Navbar renderAlert={renderAlert} />
+          <Alert alert={alert} />
+          <div className="container">
+            <Routes>
+              <Route exact path="/about" element={<About />} />
+              <Route
+                exact
+                path="/"
+                element={<Home renderAlert={renderAlert} />}
+              />
+              <Route
+                exact
+                path="/signup"
+                element={<Signup renderAlert={renderAlert} />}
+              />
+              <Route
+                exact
+                path="/login"
+                element={<Login renderAlert={renderAlert} />}
+              />
+            </Routes>
+          </div>
+        </Router>
+      </DarkState>
     </NoteState>
   );
 }
